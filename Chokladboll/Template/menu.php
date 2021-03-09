01a0187ca3c6cd89e654419e669e5a09ba8a6dd6
AlbinSundh <54435465+AlbinSundh@users.noreply.github.com>
2021-03-09 14:36:57 +0100

    Add files via upload

--- Chokladboll/Template/menu.php
+++ Chokladboll/Template/menu.php
@@ -1,4 +1,7 @@
-<nav><!--Navigationsmeny-->
+<?php
+	if(!isset($_SESSION['username'])){
+		echo<<<NAV
+		<nav>
 			<ul>
 				<li><a href="index.php">Start</a></li>
 				<li><a href="products.php">Produkter</a></li>
@@ -6,3 +9,35 @@
 				<li><a href="login.php">Logga in</a></li>
 			</ul>
 		</nav>
+NAV;
+	}
+	else{
+		if($_SESSION['status']==1){
+			echo<<<NAV
+			<nav>
+			<ul>
+				<li><a href="index.php">Start</a></li>
+				<li><a href="products.php">Produkter</a></li>
+				<li><a href="sida3.php">Varusida 2</a></li>
+				<li><a href="admin.php">Admin</a></li>
+			</ul>
+		</nav>
+		
+NAV;
+	}
+	elseif{
+		if($_SESSION['status']==2){
+			echo<<<NAV
+			<nav>
+			<ul>
+				<li><a href="index.php">Start</a></li>
+				<li><a href="products.php">Produkter</a></li>
+				<li><a href="createPoducts.php">Skapa Product</a></li>
+				<li><a href="admin.php">Admin</a></li>
+			</ul>
+		</nav>
+NAV;
+		}
+	}
+	}
+?>
--- Chokladboll/html/index.php
+++ Chokladboll/html/index.php
@@ -1,6 +1,7 @@
 <?php
 	$h1span = "Start";
 	
+	session_start();
 	header("Content-type:text/html; charset=utf-8");
 	require "../Template/index-template.php";
 	?>
